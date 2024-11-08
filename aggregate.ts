7fb7cc1774d62418885d03bab51d4df56d0c7213
Michael Telford <Michael.Telford@ibm.com>
2024-11-08 12:49:53 +0000

    Created types dir

--- aggregate.ts
+++ aggregate.ts
@@ -2,75 +2,62 @@
 // Script to retrieve and aggregate the results/jumps from all leaderboards.
 // The output is a common jump format stored in a file called `aggregated.json`.
 
-const request = require("request");
-
-type Jump = {
-    source: string,
-    position: string,
-    name: string,
-    height: string,
-    date: string,
-    location?: string,
-    gender?: string,
-    country?: string,
-    imageURL?: string,
-};
-
-type Jumps = Jump[];
+import request from "request";
+import { Jumps, Jump } from './types/types';
 
 const makeHTTPRequest = async (source: string, options) => {
-    return await new Promise<void>((resolve, reject) => {
-        request.get(options, (err, res, body) => {
-            if (err) {
-                console.error(`Failed to GET ${source} response: ${err}`);
-                return reject(err);
-            }
-            
-            console.log(`${source} HTTP status: ${res.statusCode}`);
-            return resolve(body);
-        });
+  return await new Promise<void>((resolve, reject) => {
+    request.get(options, (err, res, body) => {
+      if (err) {
+        console.error(`Failed to GET ${source} response: ${err}`);
+        return reject(err);
+      }
+      
+      console.log(`${source} HTTP status: ${res.statusCode}`);
+      return resolve(body);
     });
+  });
 };
 
 const getSurfrResults = async () => {
-    const options = {
-        url: "https://kiter-271715.appspot.com/leaderboards/list/height/alltime/0?accesstoken=e16a0f15-67c5-4306-81a5-0c554a55a222",
-        json: true
-    };
-    
-    console.log("Requesting results from Surfr...");
+  const options = {
+    url: "https://kiter-271715.appspot.com/leaderboards/list/height/alltime/0?accesstoken=e16a0f15-67c5-4306-81a5-0c554a55a222",
+    json: true
+  };
   
-    return await makeHTTPRequest("Surfr", options);
+  console.log("Requesting results from Surfr...");
+
+  return await makeHTTPRequest("Surfr", options);
 };
 
 const getWooResults = async () => {
-    const options = {
-        url: "https://prod3.apiwoo.com/leaderboardsHashtags?offset=0&page_size=50&feature=height&game_type=big_air",
-        json: true
-    };
-    
-    console.log("Requesting results from Woo...");
+  const options = {
+    url: "https://prod3.apiwoo.com/leaderboardsHashtags?offset=0&page_size=50&feature=height&game_type=big_air",
+    json: true
+  };
   
-    return await makeHTTPRequest("Woo", options);
+  console.log("Requesting results from Woo...");
+
+  return await makeHTTPRequest("Woo", options);
 };
 
 const pullSources = async () => {
-    const jumps: Jumps = [];
-    
-    await Promise.all([getSurfrResults(), getWooResults()]).then(values => {
-        const [surfrBody, wooBody] = values;
-        console.log("Pulled sources");
-    });
-    
-    return jumps;
+  const jumps: Jumps = [];
+  
+  await Promise.all([getSurfrResults(), getWooResults()]).then(values => {
+    const [surfrBody, wooBody] = values;
+    console.log("Pulled sources");
+  });
+  
+  return jumps;
 };
 
 const main = async () => {
-    console.log("Running aggregate script...");
-    
-    const jumps = await pullSources();
-    
-    console.log("Finished aggregate script");
+  console.log("Running aggregate script...");
+  
+  const jumps = await pullSources();
+  
+  console.log("Finished aggregate script");
 }
 
 main();
---
+++ types/types.ts
@@ -0,0 +1,14 @@
+
+export type Jump = {
+    source: string,
+    position: string,
+    name: string,
+    height: string,
+    date: string,
+    location?: string,
+    gender?: string,
+    country?: string,
+    imageURL?: string,
+};
+
+export type Jumps = Jump[];
